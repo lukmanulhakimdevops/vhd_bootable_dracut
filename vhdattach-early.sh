e4535567d14fe6f1fd77a890eed0f98917cd7fcb
lukmanulhakimdevops <lukmanul.hakim.devops@gmail.com>
2025-09-25 23:27:39 +0700

    Delete vhdattach-early.sh

--- vhdattach-early.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-# vhdattach-early.sh - pre-mount fallback (keeps same behavior)
-set -e
-# (content: same logic as vhdattach-initqueue.sh)
-# For brevity in this file, we symlink to the initqueue script at install time.
-exec /usr/lib/dracut/modules.d/90vhdattach/vhdattach-initqueue.sh
