666a1e77c0e5af92d6b48a4baab685f04015ced5
lukmanulhakimdevops <lukmanul.hakim.devops@gmail.com>
2025-10-17 10:06:46 +0700

    Update module-setup.sh

--- module-setup.sh
+++ module-setup.sh
@@ -1,7 +1,25 @@
+#!/bin/bash
 # ============================================================
-# Configuration for vhdattach (Dracut 108+)
+# Dracut 108+ module-setup.sh
 # ============================================================
-hostonly="no"
-add_drivers+=" loop ntfs3 dm_mod dm_snapshot "
-add_dracutmodules+=" vhdattach "
-dracut_log_level="debug"
+
+check() { return 0; }
+
+depends() {
+    echo "dm"
+    return 0
+}
+
+install() {
+    # Tools penting yang harus dibawa ke initramfs
+    inst_multiple \
+        losetup kpartx partx mount blkid udevadm findmnt partprobe \
+        sleep dmesg mkdir tee basename dirname cat lsblk
+
+    # Kernel drivers untuk NTFS & loop
+    instmods loop ntfs3 dm_mod dm_snapshot
+
+    # Hooks
+    inst_hook pre-mount 05 "$moddir/vhdattach-early.sh"
+    inst_hook initqueue/settled 90 "$moddir/vhdattach-initqueue.sh"
+}
